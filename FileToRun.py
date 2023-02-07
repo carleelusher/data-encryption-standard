ad5f335f5128ef108e8e1cfc89d014ea33c9802b
Rohit Chivukula <rohitchivukula9@gmail.com>
2023-02-07 14:31:40 -0500

    Implemented xor, intial_permutation, inverse_initial_permutation and added comments. Fixed error in generating blocks of 64 bits.

--- FileToRun.py
+++ FileToRun.py
@@ -53,41 +53,35 @@ S = [[[14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
       [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
       [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],
       [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]],
-
      [[15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10],
       [3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5],
       [0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15],
       [13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9]],
-
      [[10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8],
       [13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1],
       [13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7],
       [1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12]],
-
      [[7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15],
       [13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9],
       [10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4],
       [3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14]],
-
      [[2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9],
       [14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6],
       [4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14],
       [11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3]],
-
      [[12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11],
       [10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8],
       [9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6],
       [4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13]],
-
      [[4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1],
       [13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6],
       [1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2],
       [6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12]],
-
      [[13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7],
       [1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2],
       [7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8],
-      [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11]]]
+      [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11]]
+     ]
 
 # Straight Permutation Table
 P = [16, 7, 20, 21,
@@ -100,9 +94,37 @@ P = [16, 7, 20, 21,
      22, 11, 4, 25]
 
 
+def xor(block1: list, block2: list) -> list:
+    """
+    Performs the XOR operation on two blocks of same dimensions
+    :param block1: list of first operand
+    :param block2: list of second operand
+    :return: block with result of XOR operation
+    """
+    if len(block1) != len(block2):
+        print("Invalid operation. Inputs need to have same dimensions.")
+        return
+
+    return [a ^ b for a, b in zip(block1, block2)]
+
+def initial_permutation(block: list) -> list:
+    """
+    Perform initial permutation on the block using IP table
+    :arg block: list of 64 bits
+    :return: list of 64 bits after permutation
+    """
+    return [block[x - 1] for x in IP]
 
 
-def process_input_text(filename: str) -> list:
+def inverse_initial_permutation(block: list) -> list:
+    """
+    Perform inverse initial permutation on the block using IP^-1 table
+    :arg block: list of 64 bits
+    :return: list of 64 bits after inverse initial permutation
+    """
+    return [block[x - 1] for x in IP_inverse]
+
+def process_plain_text(filename: str) -> list:
     """
     Converts text file into a list of 64-bit blocks
     :param filename: string name of text file (.txt)
@@ -124,15 +146,17 @@ def process_input_text(filename: str) -> list:
     # list containing only bits
     bits = [bit for byte in bits for bit in byte]
 
+    # blocks = []
+    # for i in range(0, len(bits), 64):
+    #     blocks.append(bits[i:i+64])
+
     # list containing blocks of 64 bits
-    blocks = [[] for block in range(len(bits)//64)]
-    for i in bits:
-        blocks[bits.index(i)//64].append(i)
+    blocks = [bits[i:i + 64] for i in range(0, len(bits), 64)]
 
     return blocks
 
 
-def process_input_password(password: str) -> list:
+def generate_key(password: str) -> list:
     """
     Converts password into a 64-bit key
     :param password: string user input
@@ -175,6 +199,10 @@ def input_password() -> str:
     return password
 
 
-# process_input_password('password')
+def main():
+    # password = input_password()
+    # key = generate_key(password)
+    key = generate_key('password')
+    blocks = process_plain_text('plaintext.txt')
 
-process_input_text('plaintext.txt')
+main()
